cabdec7a1e9b5839c142112776f8b5165301b7bb
HanselHu <apologize0616@gmail.com>
2016-04-06 18:06:22 +0800

    作業批改完成!

---
+++ 404530029_2/404530029_2/src/EasterTester.java
@@ -0,0 +1,17 @@
+/*
+ * [B]76
+ * [TA's Advise]
+ * 1.Please follow the principle of assignment to implement your homework {e.g. function name)
+ * 2.You implemented anonymous Gregorian algorithm, but not gauss algorithm, grade will start from [B]
+ * 3.You didn't complement the entire algorithm, please check it carefully.
+ * 4.There are some math method that you can use: Math.floor(), Math.abs(), Math.round(), Math.sqrt(), and so on...
+ * 5.You didn't execute javadoc to produce document about your code, -5.
+ * */
+public class EasterTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Easter.getEasterDate(2001));
+		System.out.println(Easter.getEasterDate(2012));
+	}
+}
